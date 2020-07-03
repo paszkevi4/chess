4c900a8cbff4d20ffcdb173286d14572498da5c6
paszkevi4 <paszkevi4@aol.com>
2020-07-03 14:08:27 +0300

    board logic

--- src/app/render/board.js
+++ src/app/render/board.js
@@ -1,5 +1,15 @@
+const CELL_COUNT = 8
+const ROW_COUNT = CELL_COUNT
+
+let row = new Array(CELL_COUNT).fill(`<div id="cell">1</div>`).join("");
+let board = new Array(ROW_COUNT).fill(`<div id="row">${row}</div>`).join("");
+
 const renderBoard = () => `
-    <h1>Board</h1>
+    <div id="board">
+        ${board}
+    </div>
 `
+
+console.log(board)
 
 export default renderBoard
