06be24afac4a81bcedeaed3aa28cbd3957e75e7b
paszkevi4 <paszkevi4@aol.com>
2020-07-05 02:31:48 +0300

    board is ready to play

--- src/app/render/board.js
+++ src/app/render/board.js
@@ -1,15 +1,29 @@
 const CELL_COUNT = 8
 const ROW_COUNT = CELL_COUNT
 
-let row = new Array(CELL_COUNT).fill(`<div id="cell">1</div>`).join("");
-let board = new Array(ROW_COUNT).fill(`<div id="row">${row}</div>`).join("");
+let row = (rowIndex) => {
+    return new Array(CELL_COUNT)
+        .fill(null)
+        .map((_, cellIndex) => {
+            const isBlack = rowIndex % 2 ===0 ? cellIndex % 2 !== 0 : cellIndex % 2 === 0 ;
+            return `<div class="cell ${isBlack ? 'black' : 'white'}"></div>`
+        })
+        .join("");
+}
 
-const renderBoard = () => `
-    <div id="board">
-        ${board}
-    </div>
-`
+/*let row1 = new Array(CELL_COUNT).fill(null).map(() => `<div class="cell">1</div>`).join("");*/
 
-console.log(board)
+
+const renderBoard = () => {
+    let board = new Array(ROW_COUNT)
+        .fill(null)
+        .map((_, index) => `<div class="row">${row(index)}</div>`)
+        .join("");
+    return `
+        <div id="board">
+            ${board}
+        </div>
+    `;
+}
 
 export default renderBoard
